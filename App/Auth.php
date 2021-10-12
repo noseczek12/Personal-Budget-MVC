259e88fa89489bf5face40036b29583f41be223e
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2021-10-12 22:34:35 +0200

    Added remember the login in a cookie

--- App/Auth.php
+++ App/Auth.php
@@ -15,7 +15,9 @@ class Auth
 			$_SESSION['user_id'] = $user->id;
 			
 			if($remember_me) {
-					$user->rememberLogin();
+					if($user->rememberLogin()) {
+						setcookie('remember_me' , $user->remember_token, $user->expiry_timestamp, '/');
+					}
 			}
 		}
 		
