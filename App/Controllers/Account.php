d93f3e9568c0738cb7ef07f3dfd36807e539c083
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2021-10-22 23:37:01 +0200

    Added validation of email in edit form

--- App/Controllers/Account.php
+++ App/Controllers/Account.php
@@ -12,7 +12,7 @@ class Account extends \Core\Controller
   //funkcja sprawdzająca czy email już istnieje
   public function validateEmailAction()
   {
-    $is_valid = ! User::emailExists($_GET['email']);
+    $is_valid = ! User::emailExists($_GET['email'], $_GET['ignore_id']??null);
 
     header('Content-Type: application/json');
     echo json_encode($is_valid);
