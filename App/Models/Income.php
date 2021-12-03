7c27eabc3b9d0b967cab86e0fdf3febc6cb89f04
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2021-12-03 09:52:56 +0100

    Removed income payment category

--- App/Models/Income.php
+++ App/Models/Income.php
@@ -25,15 +25,14 @@ class Income extends \Core\Model
 
         if (empty($this->errors)) {
 
-            $sql = 'INSERT INTO incomes (user_id, income_category_assigned_to_user_id, payment_method_assigned_to_user_id, amount, date_of_income, income_comment)
-                    VALUES (:user_id, :income_category, :income_payment, :income_amount, :income_date, :income_comment)';
+            $sql = 'INSERT INTO incomes (user_id, income_category_assigned_to_user_id, amount, date_of_income, income_comment)
+                    VALUES (:user_id, :income_category, :income_amount, :income_date, :income_comment)';
 
             $db = static::getDB();
             $stmt = $db->prepare($sql);
 
             $stmt->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
             $stmt->bindValue(':income_category', $this->category, PDO::PARAM_STR);
-			$stmt->bindValue(':income_payment', $this->payment, PDO::PARAM_STR);
             $stmt->bindValue(':income_amount', $this->amount, PDO::PARAM_INT);
             $stmt->bindValue(':income_date', $this->date, PDO::PARAM_STR);
 			$stmt->bindValue(':income_comment', $this->comment, PDO::PARAM_STR);
@@ -47,10 +46,6 @@ class Income extends \Core\Model
 	//funkcja sprawdzająca dane przed dodaniem do bazy
 	public function validate()
 	{
-		// kategoria przychodu
-       if ($this->category == 'Wybierz...') {
-           $this->errors[] = 'Należy wybrać kategorię !';
-       }
 
 	   // kwota przychodu
        if ($this->amount == 0 || $this->amount < 0) {
