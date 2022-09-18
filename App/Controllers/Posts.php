972815021c6f2edeae71c45ec6a93757c37bd532
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2022-09-19 00:00:24 +0200

    Poprawka PSR dla Posts.php

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -1,34 +1,44 @@
 <?php
 
 namespace App\Controllers;
-use \Core\View;
-use App\Models\Post;
 
-/* Posts Controller */
+use App\Models\Post;
+use Core\View;
+
+/**
+ * * Logout function.
+ */
 class Posts extends \Core\Controller
 {
-
-    // funkcja wyświetlająca stronę index 
+    /**
+     * * Function showing index page.
+     */
     public function indexAction()
     {
-		$posts = Post::getAll();
-		
-       View::renderTemplate('Posts/index.html', [
-					'posts' => $posts
-		]);
-    }
+        $posts = Post::getAll();
 
-     // funkcja wyświetlająca stronę add-new 
+        View::renderTemplate(
+            'Posts/index.html',
+            ['posts' => $posts]
+        );
+    }// end indexAction()
+
+    /**
+     * * Function showing add new post page.
+     */
     public function addNewAction()
     {
         echo 'Hello from the addNew action in the Posts controller!';
-    }
-	
-	// funkcja wyświetlająca stronę edit 
-	 public function editAction()
+    }// end addNewAction()
+
+    // funkcja wyświetlająca stronę edit
+    public function editAction()
     {
         echo 'Hello from the edit action in the Posts controller!';
-        echo '<p>Route parameters: <pre>' .
-             htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
-    }
-}
+        echo '<p>Route parameters: <pre>'.
+        htmlspecialchars(print_r(
+            $this->route_params,
+            true
+        )).'</pre></p>';
+    }// end editAction()
+}// end class
