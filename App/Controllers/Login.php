29e9dafbce59be9a640d58dac7b82eef45c228a1
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2022-09-18 23:46:34 +0200

    Poprawka PSR dla Login.php

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -2,52 +2,69 @@
 
 namespace App\Controllers;
 
-use PDO;
-use \Core\View;
-use \App\Models\User;
-use \App\Auth;
-use \App\Flash;
+use App\Auth;
+use App\Flash;
+use App\Models\User;
+use Core\View;
 
-
-//Login controller
-
+/**
+ * * Login Controller.
+ */
 class Login extends \Core\Controller
 {
-	//funkcja pokazująca stronę logowania
-	public function newAction()
-	{
-			View::renderTemplate('Login/new.html');
-	}
-	
-	//funkcja logowania użytkownika
-	public function createAction()
-	{
-			$user = User::authenticate($_POST['email'] , $_POST['password']);
-			$remember_me = isset($_POST['remember_me']);
-			
-			if($user){
-					Auth::login($user, $remember_me);
-					Flash::addMessage('Zalogowano pomyślnie.');
-					$this->redirect(Auth::getReturnToPage());
-			}else {
-					Flash::addMessage('Nie udało się zalogować. Spróbuj ponownie.' , Flash::WARNING);
-					View::renderTemplate('Login/new.html' , ['email' => $_POST['email'], 'remember_me' => $remember_me]);
-			}
-	}
-	
-	// funkcja wylogowania
-	 public function destroyAction()
+    /**
+     * * Function showing login page.
+     */
+    public function newAction()
+    {
+        View::renderTemplate('Login/new.html');
+    }// end newAction();
+
+    /**
+     * * Loggin user function.
+     */
+    public function createAction()
+    {
+        $user = User::authenticate(
+            $_POST['email'],
+            $_POST['password']
+        );
+        $remember_me = isset($_POST['remember_me']);
+        if ($user) {
+            Auth::login(
+                $user,
+                $remember_me
+            );
+            Flash::addMessage('Zalogowano pomyślnie.');
+            $this->redirect(Auth::getReturnToPage());
+        } else {
+            Flash::addMessage(
+                'Nie udało się zalogować. Spróbuj ponownie.',
+                Flash::WARNING
+            );
+            View::renderTemplate(
+                'Login/new.html',
+                ['email' => $_POST['email'],
+                    'remember_me' => $remember_me, ]
+            );
+        }
+    }// end createAction()
+
+    /**
+     * * Logout function.
+     */
+    public function destroyAction()
     {
         Auth::logout();
-
         $this->redirect('/login/show-logout-message');
-    }
+    }// end destroyAction()
 
-   //pokazanie wiadomości o wylogowaniu
+    /**
+     * * Show logout message.
+     */
     public function showLogoutMessageAction()
     {
-      Flash::addMessage('Wylogowano pomyślnie.');
-
-      $this->redirect(Auth::getReturnToPage());
-    }
-}
+        Flash::addMessage('Wylogowano pomyślnie.');
+        $this->redirect(Auth::getReturnToPage());
+    }// end showLogoutMessageAction()
+}// end class
