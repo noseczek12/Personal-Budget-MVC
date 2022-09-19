0f6a9a52f3c04bab7c61783242f45f50f0123fdb
Tomasz Nosol <tomasz.nosol@wskz.pl>
2022-09-19 09:29:36 +0200

    Poprawka PSR dla ShowBalance.php

--- App/Controllers/ShowBalance.php
+++ App/Controllers/ShowBalance.php
@@ -2,84 +2,123 @@
 
 namespace App\Controllers;
 
-use \Core\View;
-use \App\Models\Balance;
+use Core\Controller;
+use Core\View;
+use App\Models\Balance;
 
-// Balance Controller
+/**
+ * * ShowBalance Controller.
+ */
 
-class ShowBalance extends \Core\Controller
+class ShowBalance extends Controller
 {
-
-    //pokazuje bilans użytkownika z bieżącego miesiąca (opcja domyślna)
+    /**
+     * * Show user current month balance (default option).
+     */
     public function showCurrentMonthAction()
     {
         $first_day = date('Y-m-01');
         $last_day = date('Y-m-t');
-        $period_info = "Okres czasu od ".$first_day." do ".$last_day;
-        $period = "BETWEEN '".$first_day."' AND '".$last_day."'";
-        $incomesArray=Balance::getPieChartIncomes($period);
-        $expensesArray=Balance::getPieChartExpenses($period);
-        
-        View::renderTemplate('Balance/Show.html',
-        array('incomes'=> Balance::getAllIncomes($period), 'expenses'=> Balance::getAllExpenses($period), 
-        'jsonincomes'=>json_encode($incomesArray), 'jsonexpenses'=>json_encode($expensesArray),
-        'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)), 'expensesSum'=> Balance::calcSum(Balance::getAllExpenses($period)),
-        'period_info' => $period_info, 'detailedExpenses'=>Balance::getDetailedExpenses($period),
-        'detailedIncomes'=>Balance::getDetailedIncomes($period)));
+        $period_info = "Okres czasu od " . $first_day . " do " . $last_day;
+        $period = "BETWEEN '" . $first_day . "' AND '" . $last_day . "'";
+        $incomesArray = Balance::getPieChartIncomes($period);
+        $expensesArray = Balance::getPieChartExpenses($period);
+
+        View::renderTemplate(
+            'Balance/Show.html',
+            array(
+                'incomes' => Balance::getAllIncomes($period),
+                'expenses' => Balance::getAllExpenses($period),
+                'jsonincomes' => json_encode($incomesArray),
+                'jsonexpenses' => json_encode($expensesArray),
+                'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)),
+                'expensesSum' => Balance::calcSum(Balance::getAllExpenses($period)),
+                'period_info' => $period_info,
+                'detailedExpenses' => Balance::getDetailedExpenses($period),
+                'detailedIncomes' => Balance::getDetailedIncomes($period)
+            )
+        );
     }
 
-    //pokazuje bilans użytkownika z poprzedniego miesiąca
+    /**
+     * * Show user previous month balance.
+     */
     public function showPreviousMonthAction()
     {
         $first_day = date('Y-m-d', strtotime('first day of last month'));
         $last_day = date('Y-m-d', strtotime('last day of last month'));
-        $period_info = "Okres czasu od ".$first_day." do ".$last_day;
-        $period = "BETWEEN '".$first_day."' AND '".$last_day."'";
-        $incomesArray=Balance::getPieChartIncomes($period);
-        $expensesArray=Balance::getPieChartExpenses($period);
-        
-        View::renderTemplate('Balance/Show.html',
-        array('incomes'=> Balance::getAllIncomes($period), 'expenses'=> Balance::getAllExpenses($period), 
-        'jsonincomes'=>json_encode($incomesArray), 'jsonexpenses'=>json_encode($expensesArray),
-        'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)), 'expensesSum'=> Balance::calcSum(Balance::getAllExpenses($period)),
-        'period_info' => $period_info, 'detailedExpenses'=>Balance::getDetailedExpenses($period),
-        'detailedIncomes'=>Balance::getDetailedIncomes($period)));
+        $period_info = "Okres czasu od " . $first_day . " do " . $last_day;
+        $period = "BETWEEN '" . $first_day . "' AND '" . $last_day . "'";
+        $incomesArray = Balance::getPieChartIncomes($period);
+        $expensesArray = Balance::getPieChartExpenses($period);
+
+        View::renderTemplate(
+            'Balance/Show.html',
+            array(
+                'incomes' => Balance::getAllIncomes($period),
+                'expenses' => Balance::getAllExpenses($period),
+                'jsonincomes' => json_encode($incomesArray),
+                'jsonexpenses' => json_encode($expensesArray),
+                'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)),
+                'expensesSum' => Balance::calcSum(Balance::getAllExpenses($period)),
+                'period_info' => $period_info,
+                'detailedExpenses' => Balance::getDetailedExpenses($period),
+                'detailedIncomes' => Balance::getDetailedIncomes($period)
+            )
+        );
 
     }
 
-    //pokazuje bilans użytkownika z bieżącego roku
+    /**
+     * * Show user current year balance.
+     */
     public function showCurrentYearAction()
     {
         $first_day = date('Y-m-d', strtotime('first day of January'));
         $last_day = date('Y-m-d', strtotime('last day of December'));
-        $period_info = "Okres czasu od ".$first_day." do ".$last_day;
-        $period = "BETWEEN '".$first_day."' AND '".$last_day."'";
-        $incomesArray=Balance::getPieChartIncomes($period);
-        $expensesArray=Balance::getPieChartExpenses($period);
-        
-        View::renderTemplate('Balance/Show.html',
-        array('incomes'=> Balance::getAllIncomes($period), 'expenses'=> Balance::getAllExpenses($period), 
-        'jsonincomes'=>json_encode($incomesArray), 'jsonexpenses'=>json_encode($expensesArray),
-        'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)), 'expensesSum'=> Balance::calcSum(Balance::getAllExpenses($period)),
-        'period_info' => $period_info, 'detailedExpenses'=>Balance::getDetailedExpenses($period),
-        'detailedIncomes'=>Balance::getDetailedIncomes($period)));
-    }
+        $period_info = "Okres czasu od " . $first_day . " do " . $last_day;
+        $period = "BETWEEN '" . $first_day . "' AND '" . $last_day . "'";
+        $incomesArray = Balance::getPieChartIncomes($period);
+        $expensesArray = Balance::getPieChartExpenses($period);
 
-    //pokazuje bilans użytkownika z wybranego okresu czasu
+        View::renderTemplate(
+            'Balance/Show.html',
+            array(
+                'incomes' => Balance::getAllIncomes($period),
+                'expenses' => Balance::getAllExpenses($period),
+                'jsonincomes' => json_encode($incomesArray),
+                'jsonexpenses' => json_encode($expensesArray),
+                'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)),
+                'expensesSum' => Balance::calcSum(Balance::getAllExpenses($period)),
+                'period_info' => $period_info,
+                'detailedExpenses' => Balance::getDetailedExpenses($period),
+                'detailedIncomes' => Balance::getDetailedIncomes($period)
+            )
+        );
+    }
+    /**
+     * * Show user custom period balance.
+     */
     public function showCustomAction()
     {
-        $period_info = "Okres czasu od ".$_POST['startDate']." do ".$_POST['endDate'];
-        $period = "BETWEEN '".$_POST['startDate']."' AND '".$_POST['endDate']."'";
-        $incomesArray=Balance::getPieChartIncomes($period);
-        $expensesArray=Balance::getPieChartExpenses($period);
-        
-        View::renderTemplate('Balance/Show.html',
-        array('incomes'=> Balance::getAllIncomes($period), 'expenses'=> Balance::getAllExpenses($period), 
-        'jsonincomes'=>json_encode($incomesArray), 'jsonexpenses'=>json_encode($expensesArray),
-        'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)), 'expensesSum'=> Balance::calcSum(Balance::getAllExpenses($period)),
-        'period_info' => $period_info, 'detailedExpenses'=>Balance::getDetailedExpenses($period),
-        'detailedIncomes'=>Balance::getDetailedIncomes($period)));
+        $period_info = "Okres czasu od " . $_POST['startDate'] . " do " . $_POST['endDate'];
+        $period = "BETWEEN '" . $_POST['startDate'] . "' AND '" . $_POST['endDate'] . "'";
+        $incomesArray = Balance::getPieChartIncomes($period);
+        $expensesArray = Balance::getPieChartExpenses($period);
+
+        View::renderTemplate(
+            'Balance/Show.html',
+            array(
+                'incomes' => Balance::getAllIncomes($period),
+                'expenses' => Balance::getAllExpenses($period),
+                'jsonincomes' => json_encode($incomesArray),
+                'jsonexpenses' => json_encode($expensesArray),
+                'incomesSum' => Balance::calcSum(Balance::getAllIncomes($period)),
+                'expensesSum' => Balance::calcSum(Balance::getAllExpenses($period)),
+                'period_info' => $period_info,
+                'detailedExpenses' => Balance::getDetailedExpenses($period),
+                'detailedIncomes' => Balance::getDetailedIncomes($period)
+            )
+        );
     }
-
-
 }
