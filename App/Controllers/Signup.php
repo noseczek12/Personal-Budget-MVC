950014a8d54313acda28ae1ec042e90ae30013b6
Tomasz Nosol <tomasz.nosol.programista@gmail.com>
2022-09-19 23:21:44 +0200

    Poprawka PSR dla Signup.php

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -5,44 +5,58 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\User;
 
-/* Signup controller */
+/** 
+ * Signup controller.
+ **/
 class Signup extends \Core\Controller
 {
-//funkcja wyświetlająca stronę new
+    /** 
+     * Function showing Signup Page.
+     **/
     public function newAction()
     {
         View::renderTemplate('Signup/new.html');
     }
-	
-	public function createAction()
-	{
-		$user = new User($_POST);
+
+    /** 
+     * Function creating new user.
+     **/
+    public function createAction()
+    {
+        $user = new User($_POST);
         if ($user->save()) {
-			$user->sendActivationEmail();
-			$this->redirect('/signup/success');
-       } else {
-            View::renderTemplate('Signup/new.html', [
-                'user' => $user
-            ]);
-       }
-	}
-	
-	//wyświetlenie strony przy sukcesie rejestracji
-	public function successAction()
-	{
-		View::renderTemplate('Signup/success.html');
-	}
-	
-	//aktywacja nowego konta
-	public function activateAction()
-	{
-			User::activate($this->route_params['token']);
-			$this->redirect('/signup/activated');
-	}
-	
-	//wyświetlenie strony przy sukcesie aktywacji
-	public function activatedAction()
-	{
-		View::renderTemplate('Signup/activated.html');
-	}
+            $user->sendActivationEmail();
+            $this->redirect('/signup/success');
+        } else {
+            View::renderTemplate(
+                'Signup/new.html',
+                ['user' => $user]
+            );
+        }
+    }
+
+    /** 
+     * Shows signup success action.
+     **/
+    public function successAction()
+    {
+        View::renderTemplate('Signup/success.html');
+    }
+
+    /** 
+     * New account activation.
+     **/
+    public function activateAction()
+    {
+        User::activate($this->route_params['token']);
+        $this->redirect('/signup/activated');
+    }
+
+    /** 
+     * Shows page during signup success.
+     **/
+    public function activatedAction()
+    {
+        View::renderTemplate('Signup/activated.html');
+    }
 }
