1ca07c142e512ca4d803723766e7776ebd47fbd5
Tomasz Nosol <tomasz.nosol@wskz.pl>
2022-09-20 10:28:17 +0200

    Refaktoryzacja PSR dla Token.php

--- App/Token.php
+++ App/Token.php
@@ -2,30 +2,36 @@
 
 namespace App;
 
-//unikalne losowe tokeny
+/**
+ * * Generating unique random tokens
+ */
 
 class Token
 {
-	protected $token;
-	
-	public function __construct($token_value = null)
-	{
-			if ($token_value){
-					$this->token = $token_value;
-			}else{
-					$this->token = bin2hex(random_bytes(16));
-			}
-	}
-	
-	//zwróc wartość tokena
-	public function getValue()
-	{
-			return $this->token;
-	}
-	
-	//zwróć zhashowany token za pomoca HMAC
-	public function getHash()
-	{
-			return hash_hmac('sha256', $this->token, \App\Config_new::SECRET_KEY);
-	}
+    protected $token;
+
+    public function __construct($token_value = null)
+    {
+        if ($token_value) {
+            $this->token = $token_value;
+        } else {
+            $this->token = bin2hex(random_bytes(16));
+        }
+    }
+
+    /**
+     * * Get token value.
+     */
+    public function getValue()
+    {
+        return $this->token;
+    }
+
+    /**
+     * * Get hashed token with HMAC.
+     */
+    public function getHash()
+    {
+        return hash_hmac('sha256', $this->token, Config_new::SECRET_KEY);
+    }
 }
